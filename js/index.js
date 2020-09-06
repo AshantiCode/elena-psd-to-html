403d6719364f5297e795c2135d49e8531319bf30
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-06 11:50:11 +0200

    fix padding on logo

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.cssText = "padding: 0px 10px; display: static; background: rgba(0,0,0,0.6)"
+    document.getElementById("navbar").style.cssText = "padding: 0px 20px; display: static; background: rgba(0,0,0,0.6)"
     document.getElementById("navbarResponsive").style.cssText = "padding: 0px 10px; display: static;"
     document.getElementById("navbarBrand").style.cssText = "margin-bottom: 15px;"
 		document.getElementById("logo").style.cssText = "width: 100px";
